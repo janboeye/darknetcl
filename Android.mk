a75d9f550ddf97121c9b86b6080dd069a53e89e1
Janboe Ye <yeyuanbo@xiaomi.com>
2017-10-10 19:31:40 +0800

    separate into library and executable file
    Signed-off-by: Janboe Ye <yeyuanbo@xiaomi.com>

--- Android.mk
+++ Android.mk
@@ -42,12 +42,9 @@ LOCAL_SRC_FILES :=  \
     src/option_list.c  \
     src/blas_kernels.c  \
     src/rnn_layer.c  \
-    src/swag.c  \
     src/layer.c  \
     src/batchnorm_layer.c  \
-    src/cifar.c  \
     src/crop_layer.c  \
-    src/super.c  \
     src/parser.c  \
     src/image.c  \
     src/utils.c  \
@@ -56,74 +53,97 @@ LOCAL_SRC_FILES :=  \
     src/dropout_layer_kernels.c  \
     src/list.c  \
     src/tree.c  \
-    src/rnn_vid.c  \
     src/shortcut_layer.c  \
     src/reorg_layer.c  \
-    src/classifier.c  \
-    src/regressor.c  \
     src/local_layer.c  \
     src/im2col_kernels.c  \
-    src/darknet.c  \
     src/matrix.c  \
     src/normalization_layer.c  \
-    src/writing.c  \
     src/route_layer.c  \
     src/crnn_layer.c  \
     src/convolutional_layer.c  \
-    src/yolo.c  \
-    src/lsd.c  \
     src/maxpool_layer_kernels.c  \
     src/crop_layer_kernels.c  \
     src/convolutional_kernels.c  \
     src/opencl.c  \
     src/demo.c  \
-    src/detector.c  \
     src/maxpool_layer.c  \
     src/col2im.c  \
-    src/voxel.c  \
     src/dropout_layer.c  \
     src/softmax_layer.c  \
     src/data.c  \
-    src/captcha.c  \
     src/gemm.c  \
-    src/art.c  \
     src/detection_layer.c  \
     src/activation_kernels.c  \
     src/activation_layer.c  \
     src/avgpool_layer.c  \
-    src/coco.c  \
     src/cost_layer.c  \
     src/avgpool_layer_kernels.c  \
-    src/compare.c  \
-    src/tag.c  \
     src/gru_layer.c  \
-    src/rnn.c  \
     src/deconvolutional_kernels.c  \
-    src/dice.c  \
     src/region_layer.c  \
     src/network_kernels.c  \
-    src/go.c  \
-    src/nightmare.c  \
     src/network.c  \
     src/box.c  \
     src/blas.c  \
-    src/im2col.c  \
- 
-
+    src/im2col.c
 
 LOCAL_SHARED_LIBRARIES :=  \
 	libOpenCL \
 	libclblast
 
-LOCAL_MODULE := darknetcl
+LOCAL_MODULE := libdarknetcl
 
 LOCAL_MODULE_TAGS := eng optional tests
 
 # Mark source files as dependent on Android.mk
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
+include $(BUILD_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_C_INCLUDES :=  \
+	$(OPENCL_HEADER_PATH) \
+	$(LOCAL_PATH)/include \
+	$(LOCAL_PATH)/../CLBlast/include \
+	$(LOCAL_PATH)/src
+
+LOCAL_CFLAGS := \
+          -DGPU -DOPENCL -Wall -Wfatal-errors -Wno-unused-result -std=c99 -D_BSD_SOURCE -Ofast -DGPU -DOPENCL
+
+
+LOCAL_SRC_FILES :=  \
+    src/darknet.c  \
+    src/nightmare.c  \
+    src/writing.c  \
+    src/detector.c  \
+    src/dice.c  \
+    src/classifier.c  \
+    src/regressor.c  \
+    src/compare.c  \
+    src/rnn_vid.c  \
+    src/rnn.c  \
+    src/go.c  \
+    src/cifar.c  \
+    src/art.c  \
+    src/super.c  \
+    src/captcha.c  \
+    src/swag.c  \
+    src/yolo.c  \
+    src/lsd.c  \
+    src/coco.c  \
+    src/tag.c  \
+    src/voxel.c  \
+
+LOCAL_SHARED_LIBRARIES :=  \
+	libdarknetcl
+
+LOCAL_MODULE := darknetcl
+
+LOCAL_MODULE_TAGS := eng optional tests
 
 include $(BUILD_EXECUTABLE)
-
 #-----------------
 # libOpenCL (stub)
 
